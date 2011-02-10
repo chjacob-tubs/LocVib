6d92ae90af8dd03252e7d740211f301a26cdeb24
jacob <jacob@6a27d386-5446-49e2-af76-5421be117d89>
2011-02-10 16:12:13 +0000

    Make SNFResults work in intensities-only mode
    git-svn-id: svn+ssh://tclogin.ethz.ch/home/raid/jacob/share/svn/VibTools/trunk@205 6a27d386-5446-49e2-af76-5421be117d89

--- PySNF.py
+++ PySNF.py
@@ -5,7 +5,7 @@
 import numpy 
 import math
 
-import Constants
+from Constants import *
 
 from Molecule import VibToolsMolecule
 from Modes    import VibModes
@@ -19,6 +19,9 @@ class SNFRestartFile (object) :
         self.natoms = None
         self.iaus   = None
         self.displ  = None
+
+        self.intonly = False
+        self.nmodes  = 0
 
     def read (self, filename='restart') :
         f = file(filename, 'r')
@@ -35,12 +38,23 @@ class SNFRestartFile (object) :
         self.nvar   = line[4]
         self.natoms = self.nvar / 3
         self.iaus   = line[5]
-        
+        self.nmodes = line[6]
+        if self.nmodes > 0 :
+            self.intonly = True
+
+        # number of sets of calculations 
+        # normal run: one set are the 6 Cartesian displacements
+        # intonly mode: one set are the two displacements per normal mode
+        if self.intonly :
+            self.ncalcsets = self.nmodes
+        else :
+            self.ncalcsets = self.natoms
+
         self.displ = float(f.readline().replace('D', 'E'))
         f.readline()  # fourth line: some other path
         f.readline()  # fifth line: and yet another path
 
-        for i in xrange(self.natoms) :
+        for i in xrange(self.ncalcsets) :
             line = f.readline()
             line = [int(i) for i in line.split()]
             for i in line[1:] :
@@ -53,7 +67,8 @@ class SNFRestartFile (object) :
         #        automatically detects which sections are present .
             
         self.dipole   = self._read_section(f, 3)
-        self.gradient = self._read_section(f, self.nvar)
+        if not self.intonly :
+            self.gradient = self._read_section(f, self.nvar)
         self.pollen   = self._read_section(f, 8)
         self.polvel   = self._read_section(f, 8)
         self.gtenlen  = self._read_section(f, 9)
@@ -64,15 +79,18 @@ class SNFRestartFile (object) :
 
     def _read_section(self, f, ncomp) :
 
-        nread = ncomp * self.iaus * 3
+        if self.intonly :
+            nread = ncomp * self.iaus 
+        else :
+            nread = ncomp * self.iaus * 3
 
-        result = numpy.zeros((self.natoms, nread))
+        result = numpy.zeros((self.ncalcsets, nread))
         
         nlines = nread / self.n_per_line
         if nlines*self.n_per_line < nread:
             nlines = nlines + 1
 
-        for i in xrange(self.natoms) :
+        for i in xrange(self.ncalcsets) :
             f.readline()
             n = 0
             for j in xrange(nlines) :
@@ -81,7 +99,7 @@ class SNFRestartFile (object) :
                     result[i,n] = float(fl)
                     n += 1
 
-        result.shape = (self.natoms, self.iaus * 3, ncomp)
+        result.shape = (self.ncalcsets, nread / ncomp, ncomp)
         return result
 
     
@@ -91,14 +109,16 @@ class SNFOutputFile (object) :
         self.modes = None
         self.lwl = None
         self.filename = filename
+        self.intonly = False
 
     def read (self, mol) :
-        natoms = mol.natoms
-        self.modes = VibModes(3*natoms-6, mol)
-
         f = file(self.filename, 'r')
         lines = f.readlines()
         f.close()
+
+        for l in lines :
+            if 'intensities-only-mode' in l :
+                self.intonly = True
 
         first = 0
         last  = 0
@@ -113,53 +133,100 @@ class SNFOutputFile (object) :
                 break
             if 'Raman Optical Activity properties for freq.' in l :
                 self.lwl = float(l[46:57])
-                
-        lines = lines[first:last]
-        lines = [l for l in lines if not len(l.strip()) == 0]
-        lines = [l for l in lines if not l.startswith('1')]
 
-        normalmodes = numpy.zeros((3*natoms-6, 3*natoms))
-        freqs = numpy.zeros((3*natoms-6))
+        if not self.intonly :
+            natoms = mol.natoms
+            self.modes = VibModes(3*natoms-6, mol)
+                
+            lines = lines[first:last]
+            lines = [l for l in lines if not len(l.strip()) == 0]
+            lines = [l for l in lines if not l.startswith('1')]
+
+            normalmodes = numpy.zeros((3*natoms-6, 3*natoms))
+            freqs = numpy.zeros((3*natoms-6))
         
-        ncol = len(lines[2][10:].split())
+            ncol = len(lines[2][10:].split())
 
-        icol = -ncol
-        for i, l in enumerate(lines) :
-            irow = i % (3*natoms+2) - 2
+            icol = -ncol
+            for i, l in enumerate(lines) :
+                irow = i % (3*natoms+2) - 2
             
-            if irow == -2:
-                if not ('root' in l) :
-                    raise Exception('wrong number of lines for normal modes')
-                icol += ncol
-            else:
-                line = [float(fl) for fl in l[10:].split()]
-
-                if irow == -1:
-                    freqs[icol:icol+ncol] = line
+                if irow == -2:
+                    if not ('root' in l) :
+                        raise Exception('wrong number of lines for normal modes')
+                    icol += ncol
                 else:
-                    normalmodes[icol:icol+ncol,irow] = line
-                
-        self.modes.set_modes_mw(normalmodes)
-        self.modes.set_freqs(freqs)
+                    line = [float(fl) for fl in l[10:].split()]
 
+                    if irow == -1:
+                        freqs[icol:icol+ncol] = line
+                    else:
+                        normalmodes[icol:icol+ncol,irow] = line
+                
+            self.modes.set_modes_mw(normalmodes)
+            self.modes.set_freqs(freqs)
+
+class SNFControlFile (object) :
+
+    def __init__ (self) :
+        self.modes = None 
+
+    def read (self, mol) :
+        f = file('snf_control', 'r')
+        lines = f.readlines()
+        f.close()
+
+        for i, l in enumerate(lines) :
+             if l.startswith('$nummodes') :
+                 nmodes = int(l.split()[-1])
+                 start = i+1
+
+        natoms = mol.natoms
+
+        self.modes = VibModes(nmodes, mol)
+
+        normalmodes = numpy.zeros((nmodes, 3*natoms))
+        freqs = numpy.zeros((nmodes))
+       
+        for imode in range(nmodes) :
+            freqs[imode] = float(lines[start+imode*natoms].split()[0])
+            for i in range(natoms) :
+                x, y, z = lines[start+imode*natoms+i].split()[-3:]
+                normalmodes[imode,3*i:3*i+3] = [x, y, z]
+
+        self.modes.set_modes_c(normalmodes)
+        self.modes.set_freqs(freqs)
         
 class SNFResults (object) :
 
     def __init__ (self, outname='snf.out', restartname='restart', coordfile='coord') :
         self.mol          = VibToolsMolecule()
         self.snfoutput    = SNFOutputFile(filename=outname) 
+        self.snfcontrol   = SNFControlFile() 
         self.restartfile  = SNFRestartFile()
-
         self.restartname  = restartname
         self.coordfile    = coordfile
-        
-    freqs = property(lambda self: self.snfoutput.modes.freqs)
-    modes = property(lambda self: self.snfoutput.modes)
+
+        self.intonly      = False
+    
+    def _get_modes (self) :
+        if self.snfoutput.intonly :
+            return self.snfcontrol.modes
+        else :
+            return self.snfoutput.modes
+    
+    modes = property(_get_modes)
+    freqs = property(lambda self: self.modes.freqs)
     lwl   = property(lambda self: self.snfoutput.lwl)
         
     def read (self) :
         self.mol.read_from_coord(filename=self.coordfile)
+
         self.snfoutput.read(self.mol)
+        self.intonly = self.snfoutput.intonly
+        if self.snfoutput.intonly :
+            self.snfcontrol.read(self.mol)
+
         self.restartfile.read(filename=self.restartname)
 
     def get_mw_normalmodes (self) :
@@ -171,15 +238,16 @@ class SNFResults (object) :
     def get_tensor_mean (self, tens, ncomp=None) :
         tensor = eval('self.restartfile' + '.' + tens)
 
-        natoms = tensor.shape[0]
+        ncalcsets = tensor.shape[0]
+        nval = tensor.shape[1] / 2
         if (ncomp == None) :
             ncomp = tensor.shape[2]
 
         displ = self.restartfile.displ
 
-        mean = numpy.zeros((natoms, 3, ncomp))
+        mean = numpy.zeros((ncalcsets, nval, ncomp))
 
-        for i in range(3) :
+        for i in range(nval) :
             mean[:,i,:] = (tensor[:,2*i,:ncomp] + tensor[:,2*i+1,:ncomp]) / 2.0 
 
         return mean
@@ -191,6 +259,9 @@ class SNFResults (object) :
 
         if not (self.restartfile.iaus == 2) :
             raise Exception('Differentiation only implemented for iaus = 2')
+
+        if self.intonly :
+            raise Exception('Cartesian derivatives not available in intensities only mode')
 
         tensor = eval('self.restartfile' + '.' + tens)
 
@@ -214,23 +285,42 @@ class SNFResults (object) :
         if modes==None :
             if hasattr(self, tens+'_deriv_nm') :
                 return eval('self.'+tens+'_deriv_nm')
+        else :
+            raise Exception('Argument modes of get_tensor_deriv_nm must be None in intensities-only mode')
 
-        deriv_c  = self.get_tensor_deriv_c (tens, ncomp)
+        if self.intonly :
+            tensor = eval('self.restartfile' + '.' + tens)
 
-        ncomp = deriv_c.shape[2]
+            nmodes = tensor.shape[0]
+            if (ncomp == None) :
+                ncomp = tensor.shape[2]
 
-        if modes==None :
-            modes_c = self.get_c_normalmodes()
-        else:
-            modes_c = modes.modes_c
+            displ = self.restartfile.displ
 
-        nmodes  = modes_c.shape[0]
-        natoms  = modes_c.shape[1] / 3
+            deriv_nm = numpy.zeros((nmodes, ncomp))
+            deriv_nm[:,:] = (tensor[:,0,:ncomp] - tensor[:,1,:ncomp]) / (2.0 * displ)
 
-        deriv_nm = numpy.zeros((nmodes, ncomp))
+            modes = self.get_c_normalmodes()
+            for i in range(nmodes):
+                deriv_nm[i,:] = deriv_nm[i,:] * math.sqrt(sum(modes[i,:]**2))
 
-        for icomp in range(ncomp) :
-            deriv_nm[:, icomp] = numpy.dot(modes_c,deriv_c[:,:,icomp].reshape((3*natoms)))
+        else :
+            deriv_c  = self.get_tensor_deriv_c (tens, ncomp)
+
+            ncomp = deriv_c.shape[2]
+
+            if modes==None :
+                modes_c = self.get_c_normalmodes()
+            else:
+                modes_c = modes.modes_c
+
+            nmodes  = modes_c.shape[0]
+            natoms  = modes_c.shape[1] / 3
+
+            deriv_nm = numpy.zeros((nmodes, ncomp))
+
+            for icomp in range(ncomp) :
+                deriv_nm[:, icomp] = numpy.dot(modes_c,deriv_c[:,:,icomp].reshape((3*natoms)))
 
         if modes==None :
             setattr(self, tens+'_deriv_nm', deriv_nm)
@@ -241,8 +331,6 @@ class SNFResults (object) :
         mu = self.get_tensor_deriv_nm('dipole', modes=modes)
 
         irint = mu[:,0]*mu[:,0] + mu[:,1]*mu[:,1] + mu[:,2]*mu[:,2]
-
-        from Constants import *
 
         # convert (d\mu/dR)^2 from  (au^2/amu) to (C^2 m^2 / kg)
         irint = irint * ( (au_in_Debye/Bohr_in_Meter)**2 * (Debye_in_Cm)**2 * (1.0/amu_in_kg) )
@@ -261,7 +349,7 @@ class SNFResults (object) :
         nmodes = pol.shape[0]
 
         a2 = (1.0/3.0) * (pol[:,0] + pol[:,3] + pol[:,5])
-        a2 = (a2**2)*(Constants.Bohr_in_Angstrom**4)
+        a2 = (a2**2)*(Bohr_in_Angstrom**4)
         return a2
 
     def get_g2_invariant (self, modes=None) :
@@ -275,7 +363,7 @@ class SNFResults (object) :
                            + 6.0 * pol[:,2]**2  
                            + 6.0 * pol[:,4]**2  
                          )
-        g2 = g2 * (Constants.Bohr_in_Angstrom**4)
+        g2 = g2 * (Bohr_in_Angstrom**4)
         return g2
 
     def get_raman_int (self, modes=None) :
@@ -288,7 +376,7 @@ class SNFResults (object) :
 
         aG = (1.0/9.0) * (pol[:,0] + pol[:,3] + pol[:,5])   \
                        * (gten[:,0] + gten[:,4] + gten[:,8])
-        aG = aG*(Constants.Bohr_in_Angstrom**4) * (1 / Constants.cvel) * 1e6
+        aG = aG*(Bohr_in_Angstrom**4) * (1 / cvel) * 1e6
 
         return aG
 
@@ -305,7 +393,7 @@ class SNFResults (object) :
                     3*pol[:,2]*gten[:,6] - pol[:,5]*gten[:,0] +
                     3*pol[:,4]*gten[:,7] - pol[:,5]*gten[:,4] +
                     3*pol[:,5]*gten[:,8] - pol[:,5]*gten[:,8])
-        bG = bG*(Constants.Bohr_in_Angstrom**4) * (1 / Constants.cvel) * 1e6
+        bG = bG*(Bohr_in_Angstrom**4) * (1 / cvel) * 1e6
 
         return bG
 
@@ -321,7 +409,7 @@ class SNFResults (object) :
                                + pol[:,2]*(aten[:,25]-aten[:,21]+aten[:,3]-aten[:,4]) 
                                + pol[:,4]*(aten[:,10]-aten[:,24]+aten[:,12]-aten[:,5])
                               )
-        bA = bA*(Constants.Bohr_in_Angstrom**4) * (1 / Constants.cvel) * 1e6
+        bA = bA*(Bohr_in_Angstrom**4) * (1 / cvel) * 1e6
 
         return bA
 
@@ -330,9 +418,6 @@ class SNFResults (object) :
                            (1.0/3.0)*self.get_bA_invariant(modes=modes))
 
     def check_consistency (self) :
-        res = SNFResults()
-        res.read()
-
         print
         print "Checking consistency of SNF restart file "
         print "  Large numbers mean that the change of the polarizabilities deviates from linear behavior."
@@ -340,13 +425,14 @@ class SNFResults (object) :
 
         def check (ten) :
             if ten.startswith('pol') :
-                mean_pol = res.get_tensor_mean(ten, 6)
+                mean_pol = self.get_tensor_mean(ten, 6)
             else:
-                mean_pol = res.get_tensor_mean(ten)
-            natoms = mean_pol.shape[0]
-            ncomp = mean_pol.shape[2]
+                mean_pol = self.get_tensor_mean(ten)
+            ncalcsets = mean_pol.shape[0]
+            nval      = mean_pol.shape[1]
+            ncomp     = mean_pol.shape[2]
 
-            mean_pol = mean_pol.reshape((natoms*3,ncomp))
+            mean_pol = mean_pol.reshape((ncalcsets*nval,ncomp))
             for icomp in range(ncomp) :
                 print " %14.8f  min: %3i  max:  %3i" % \
                       (mean_pol[:,icomp].max()-mean_pol[:,icomp].min(),
