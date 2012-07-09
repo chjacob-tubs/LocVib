9441df36a1702301b63a334b10a6017d7e79d15c
jacob <jacob@6a27d386-5446-49e2-af76-5421be117d89>
2012-07-09 12:29:38 +0000

    Add copyright headers
    git-svn-id: svn+ssh://tclogin.ethz.ch/home/raid/jacob/share/svn/VibTools/trunk@214 6a27d386-5446-49e2-af76-5421be117d89

--- PySNF.py
+++ PySNF.py
@@ -1,6 +1,27 @@
+# -*- coding: utf-8 -*-
 #
-# vibrational analysis for SNF
+# This file is part of the
+# LocVib 1.0 suite of tools for the analysis for vibrational spectra.
+# Copyright (C) 2009-2012 by Christoph R. Jacob and others.
 #
+#    LocVib is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    LocVib is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with LocVib.  If not, see <http://www.gnu.org/licenses/>.
+#
+# In scientific publications using the LocVib tools, please cite:
+#   Ch. R. Jacob, J. Chem. Phys 130 (2009), 084106.
+# 
+# The most recent version of LocVib is available at
+#   http://www.christophjacob.eu/locvib.php
 
 import numpy 
 import math
