dabb0f7f39b17cd0da1fd39001ef4448a4815ee0
Michael Welzel <m.welzel@tu-bs.de>
2023-05-23 14:27:39 +0000

    Fix Sphinx documentation + tiny ci.yml update

--- doc/conf.py
+++ doc/conf.py
@@ -56,9 +56,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []#['_static']
 
+import sphinx_rtd_theme
 
-import sphinx_theme
-html_theme = 'stanford_theme'
-html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
+html_theme = "sphinx_rtd_theme"
+#import sphinx_theme
+#html_theme = 'stanford_theme'
+#html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
+#html_static_path = ['_static']
+#import sphinx_theme
+#html_theme = 'stanford_theme'
+#html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
